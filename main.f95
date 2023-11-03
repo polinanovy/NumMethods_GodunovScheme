0c91d4d0210ef405bf811fbfd005fd779a5dc5d4
Polina <103999174+polinanovy@users.noreply.github.com>
2023-11-03 16:41:34 +0300

    Update files

--- main.f95
+++ main.f95
@@ -8,10 +8,12 @@ real(8) :: C
 real(8), allocatable :: u_left(:), u_right(:)
 real(8) :: t_stop
 real(8) :: g
+real(8) :: x_left, x_right
 
-g = 5.d0/3.d0
+allocate (u_left(3), u_right(3))
 
 ! Read the following parameters from file 'INPUT':
-call InitializeParameters(u_left, u_right, t_stop, N, C)
+call InitializeParameters(u_left, u_right, t_stop, N, C, x_left, x_right)
+write(*,*) u_left, u_right, t_stop, N, C, x_left, x_right
 
 end
--- modd.f95
+++ modd.f95
@@ -1,13 +1,14 @@
 module modd
 implicit none
 real(8) :: pi = 4 * atan(1.d0)
+real(8) :: g = 5.d0/3.d0
 
 contains
 
-subroutine InitializeParameters(u_left, u_right, t_stop, N, C)
+subroutine InitializeParameters(u_left, u_right, t_stop, N, C, x_left, x_right)
 ! Subroutine for setting model parameters
 integer :: N, i
-real(8) :: C, t_stop
+real(8) :: C, t_stop, x_left, x_right
 real(8), allocatable :: u_left(:), u_right(:)
 open(unit = 1, file = 'INPUT')
 read(1, *) (u_left(i), i=1,3)
@@ -15,6 +16,26 @@ read(1, *) (u_right(i), i=1,3)
 read(1, *) t_stop
 read(1, *) N
 read(1, *) C
+read(1, *) x_left
+read(1, *) x_right
 end subroutine InitializeParameters
+
+subroutine InitializeGrid(N, C, D, x_left, x_right, x, dx, dt)   !непонятно, что с t делать
+! Subroutine for initialising the grid
+real(8) :: x_left, x_right, C, D
+integer :: N, i
+real(8) :: dx, dt, x(0:N-1)
+dx = (x_right - x_left) / (N - 1)
+!dt = C * dx / D
+! dt = dx**2 corresponds to C = D
+x(0) = x_left; x(N-1) = x_right
+do i = 1, N-2
+	x(i) = x(i-1) + dx
+enddo
+end subroutine InitializeGrid
+
+subroutine Scheme()
+
+end subroutine Scheme
 
 end module
